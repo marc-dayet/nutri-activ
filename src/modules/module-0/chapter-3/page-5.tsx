f2774279907d14af24bee77a29cd2cfbe51d54a5
Clément DOUIN <soywod@users.noreply.github.com>
2020-03-28 14:58:15 +0100

    integrate m0c3p5

---
+++ src/modules/module-0/chapter-3/page-5.tsx
@@ -0,0 +1,16 @@
+import React, {FC} from "react"
+
+import {Title} from "../../page-container"
+
+import cs from "./page-5.module.scss"
+
+const Page: FC = () => {
+  return (
+    <>
+      <Title>Les contenus clés travaillés durant le parcours Cap Bien-être</Title>
+      <img className={cs.image} src="#" alt="" />
+    </>
+  )
+}
+
+export default Page
