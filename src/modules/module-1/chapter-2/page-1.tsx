0073b556362c1d922e2c6d4ecce5be440131d9c0
Clément DOUIN <soywod@users.noreply.github.com>
2020-04-30 15:33:36 +0200

    make toc and goal responsive

--- src/modules/module-1/chapter-2/page-1.tsx
+++ src/modules/module-1/chapter-2/page-1.tsx
@@ -1,8 +1,8 @@
 import React, {FC} from "react"
 
-import {Title, Grid} from "../../page-components"
-import imgLeft from "./page-1-1.png"
-import imgRight from "./page-1-2.png"
+import {Title, Grid, Img} from "../../page-components"
+import img1 from "./page-1-1.png"
+import img2 from "./page-1-2.png"
 
 import cs from "./page-1.module.scss"
 
@@ -10,22 +10,18 @@ const Page: FC = () => {
   return (
     <>
       <Title>2. Ce que doit savoir l’animateur pour préparer le voyage</Title>
-      <Grid>
-        <div className={cs.gridItem}>
-          <img className={cs.img} src={imgLeft} alt="" />
-          <div className={cs.gridItemContentLeft}>
-            <strong>Les liens entre</strong>
-            <br />
-            le bien-être, la qualité de vie, les émotions et la santé
-          </div>
+      <Grid className={cs.grid}>
+        <Img src={img1} mode="round-lg" />
+        <div className={cs.gridItemContentLeft}>
+          <strong>Les liens entre</strong>
+          <br />
+          le bien-être, la qualité de vie, les émotions et la santé
         </div>
-        <div className={cs.gridItem}>
-          <img className={cs.img} src={imgRight} alt="" />
-          <div className={cs.gridItemContentRight}>
-            <strong>Les liens entre compétences psychosociales et ressources personnelles :</strong>
-            <br />
-            savoir les identifier et prendre conscience de leur importance dans le Bien Vieillir
-          </div>
+        <Img src={img2} mode="round-lg" />
+        <div className={cs.gridItemContentRight}>
+          <strong>Les liens entre compétences psychosociales et ressources personnelles :</strong>
+          <br />
+          savoir les identifier et prendre conscience de leur importance dans le Bien Vieillir
         </div>
       </Grid>
     </>
