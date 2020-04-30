e16d0c4b66399e37d20cd026fd5382450b0c642b
Clément DOUIN <soywod@users.noreply.github.com>
2020-04-30 17:09:08 +0200

    integrate m1c2p8

---
+++ src/modules/module-1/chapter-2/page-8.tsx
@@ -0,0 +1,32 @@
+import React, {FC} from "react"
+
+import {Title, Subtitle, Paragraph, Grid, Img} from "../../page-components"
+import img from "./page-8.png"
+
+const Page: FC = () => {
+  return (
+    <>
+      <Title>2. Ce que doit savoir l’animateur pour préparer le voyage</Title>
+      <Subtitle showBook>Les émotions</Subtitle>
+      <Grid>
+        <Img src={img} mode="avatar" />
+        <div>
+          <Paragraph>
+            Une émotion est un état affectif bref et intense. C’est une réponse psychophysiologique,
+            c’est-à-dire qu’elle fait appel tant à notre corps (sensations physiques) qu’à notre
+            esprit (mémoire), à une stimulation sensorielle ou une modification de l’environnement.
+            Les émotions peuvent être ressenties comme agréables et/ou désagréables.
+            <br />
+          </Paragraph>
+          <br />
+          <Paragraph>
+            Les émotions peuvent influencer nos systèmes biologiques, nos organes, les fonctions de
+            notre corps.
+          </Paragraph>
+        </div>
+      </Grid>
+    </>
+  )
+}
+
+export default Page
