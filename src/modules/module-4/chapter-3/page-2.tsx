61a2d817411bf62d18ccb5debb26c2c858f7f002
Clément DOUIN <soywod@users.noreply.github.com>
2020-05-13 11:00:12 +0200

    add schema m4c3p2

--- src/modules/module-4/chapter-3/page-2.tsx
+++ src/modules/module-4/chapter-3/page-2.tsx
@@ -1,40 +1,17 @@
 import React, {FC} from "react"
 
 import PageContainer from "../../page"
-import {Title, Subtitle, List, ListItem} from "../../page-components"
+import {Title, Subtitle, Paragraph, Img} from "../../page-components"
+import img from "./page-2.png"
 
 const Page: FC = () => {
   return (
     <PageContainer>
-      <Title> 3. Ce que doit transmettre l’animateur au groupe durant l’étape </Title>
+      <Title>3. Ce que doit transmettre l’animateur au groupe durant l’étape</Title>
       <Subtitle>Pour maintenir le cap vers le bien-être</Subtitle>
-      <List padder>
-        <ListItem>Développer ses ressources personnelles, c’est améliorer sa santé.</ListItem>
-        <ListItem>On peut apprendre à gérer ses émotions</ListItem>
-      </List>
-      <List padder>
-        <ListItem>On peut apprendre à repérer, accepter, comprendre, prévenir le stress.</ListItem>
-        <ListItem>
-          L’analyse et l’anticipation, le retour au calme et la recherche de soutien sont des
-          techniques efficaces contre le stress, à la portée de chacun
-        </ListItem>
-      </List>
-      <List padder>
-        <ListItem>
-          On peut travailler sa santé émotionnelle comme on travaille sa souplesse ou sa
-          musculature.
-        </ListItem>
-        <ListItem>
-          Chacun peut trouver sa propre façon de cultiver ses moments de bien-être.
-        </ListItem>
-      </List>
-      <List padder>
-        <ListItem>
-          Bien vivre avec les autres contribue à notre bien-être, il est important de pratiquer une
-          activité collective.
-        </ListItem>
-        <ListItem>Etre intégré socialement, c’est aussi être soutenu en cas de coup dur.</ListItem>
-      </List>
+      <Paragraph>
+        <Img src={img} mode="fullscreen" />
+      </Paragraph>
     </PageContainer>
   )
 }
