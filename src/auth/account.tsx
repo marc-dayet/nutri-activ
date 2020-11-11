afac0cb8b89ba831d43f56f7ad411b7c91dcc65a
Clément DOUIN <soywod@users.noreply.github.com>
2020-11-11 13:09:03 +0100

    update nav account section

--- src/auth/account.tsx
+++ src/auth/account.tsx
@@ -3,8 +3,9 @@ import useObservable from "@soywod/react-use-observable";
 
 import firebase from "./firebase";
 import {auth$} from "./context";
-import iconName from "./name.icon.svg";
-import iconLogout from "./logout.icon.svg";
+import iconName from "./name.svg";
+import iconLogout from "./logout.svg";
+import iconSeparator from "./spoon.svg";
 
 import cs from "./account.module.scss";
 
@@ -15,6 +16,7 @@ const Account: FC<AccountProps> = () => {
 
   return (
     <div className={cs.grid}>
+      <img className={cs.separator} src={iconSeparator} alt="" />
       <div className={cs.header}>MA SESSION</div>
       <img className={cs.icon} src={iconName} alt="" />
       <span>{auth.type === "authenticated" && auth.name}</span>
