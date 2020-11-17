a3edc00accc7e66c972dcee271ea145a13d78288
Clément DOUIN <soywod@users.noreply.github.com>
2020-11-17 15:06:54 +0100

    integrate m2c1p4

--- src/modules/page-components/paragraph.tsx
+++ src/modules/page-components/paragraph.tsx
@@ -1,26 +1,27 @@
-import React, {FC} from "react"
-import cn from "classnames"
+import React, {FC} from "react";
+import cn from "classnames";
 
-import book from "./subtitle-book.png"
+import book from "./subtitle-book.png";
 
-import cs from "./paragraph.module.scss"
+import cs from "./paragraph.module.scss";
 
 type PageParagraphProps = {
-  className?: string
-  align?: "left" | "center" | "right"
-  padder?: boolean
-  showBook?: boolean
-}
+  className?: string;
+  align?: "left" | "center" | "right";
+  padder?: boolean;
+  showBook?: boolean;
+  bold?: boolean;
+};
 
 const PageParagraph: FC<PageParagraphProps> = props => {
-  const {className, padder, align = "center", showBook, children} = props
+  const {className, padder, align = "center", showBook, bold = false, children} = props;
 
   return (
-    <div className={cn(cs.paragraph, cs[align], className, {[cs.padder]: padder})}>
+    <div className={cn(cs.paragraph, cs[align], className, {[cs.padder]: padder, [cs.bold]: bold})}>
       {children}
       {showBook && <img className={cs.book} src={book} alt="" />}
     </div>
-  )
-}
+  );
+};
 
-export default PageParagraph
+export default PageParagraph;
