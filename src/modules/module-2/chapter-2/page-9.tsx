96ba595dfe68d5d2fe08c603870b8ff1c6b89daf
Clément DOUIN <soywod@users.noreply.github.com>
2020-11-22 11:57:19 +0100

    integrate m2c2p9

---
+++ src/modules/module-2/chapter-2/page-9.tsx
@@ -0,0 +1,27 @@
+import React, {FC} from "react";
+
+import PageContainer from "../../page";
+import {Title, Img, Paragraph} from "../../page-components";
+import img from "./page-9.png";
+
+const Page: FC = () => {
+  return (
+    <PageContainer>
+      <Title>Les nutriments énergétiques</Title>
+      <Paragraph padder>
+        <Img src={img} mode="avatar-lg" />
+      </Paragraph>
+      <Paragraph padder bold align="left">
+        Sa valeur énergétique est liée au degré d’alcool. Plus il est élevé, plus le nombre de
+        calories par gramme est conséquent.
+      </Paragraph>
+      <Paragraph align="left">
+        Contrairement aux autres nutriments il n’apporte aucun élément essentiel à notre organisme
+        et, consommé excessivement, impacte négativement notre santé Lactose et galactose (produits
+        laitiers)
+      </Paragraph>
+    </PageContainer>
+  );
+};
+
+export default Page;
