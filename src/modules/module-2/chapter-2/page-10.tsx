f870860c3dd982077c6e5d88fcd9d26c80cbe0b9
Clément DOUIN <soywod@users.noreply.github.com>
2020-11-22 11:59:46 +0100

    integrate m2c2p10

---
+++ src/modules/module-2/chapter-2/page-10.tsx
@@ -0,0 +1,33 @@
+import React, {FC} from "react";
+
+import PageContainer from "../../page";
+import {Title, Img, Grid, Paragraph, List, ListItem} from "../../page-components";
+import img from "./page-10.png";
+
+import cs from "./page-5.module.scss";
+
+const Page: FC = () => {
+  return (
+    <PageContainer>
+      <Title>Les nutriments non énergétiques</Title>
+      <Paragraph padder>
+        <Img src={img} mode="avatar-lg" />
+      </Paragraph>
+      <Grid position="left top">
+        <div className={cs.grid}>
+          <Paragraph padder bold align="left">
+            Acides gras saturés
+          </Paragraph>
+          <List>
+            <ListItem>Fruits</ListItem>
+            <ListItem>Légumes</ListItem>
+            <ListItem>Produits céréaliers</ListItem>
+            <ListItem>Légumineuses</ListItem>
+          </List>
+        </div>
+      </Grid>
+    </PageContainer>
+  );
+};
+
+export default Page;
