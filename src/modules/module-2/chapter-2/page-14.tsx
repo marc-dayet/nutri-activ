2abd4e216f911e2892e3e5e79e258ffb7d9d69da
Clément DOUIN <soywod@users.noreply.github.com>
2020-11-22 12:14:53 +0100

    integrate m2c2p14

---
+++ src/modules/module-2/chapter-2/page-14.tsx
@@ -0,0 +1,18 @@
+import React, {FC} from "react";
+
+import PageContainer from "../../page";
+import {Title, Paragraph, Img} from "../../page-components";
+import img from "./page-14.png";
+
+const Page: FC = () => {
+  return (
+    <PageContainer>
+      <Title>Les 9 repères du Programme National Nutrition Santé</Title>
+      <Paragraph>
+        <Img src={img} mode="fullscreen" />
+      </Paragraph>
+    </PageContainer>
+  );
+};
+
+export default Page;
