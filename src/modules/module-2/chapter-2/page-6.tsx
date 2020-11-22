1f761dadc8888bf82b94cbf07cf7ba89c0c98a7d
Clément DOUIN <soywod@users.noreply.github.com>
2020-11-22 11:49:15 +0100

    integrate m2c2p6

---
+++ src/modules/module-2/chapter-2/page-6.tsx
@@ -0,0 +1,33 @@
+import React, {FC} from "react";
+
+import PageContainer from "../../page";
+import {Title, Img, Grid, Paragraph, List, ListItem} from "../../page-components";
+import img from "./page-6.png";
+
+import cs from "./page-5.module.scss";
+
+const Page: FC = () => {
+  return (
+    <PageContainer>
+      <Title>Les nutriments énergétiques</Title>
+      <Paragraph padder>
+        <Img src={img} mode="avatar-lg" />
+      </Paragraph>
+      <Grid position="left top">
+        <div className={cs.grid}>
+          <Paragraph padder bold align="left">
+            Acides gras saturés (AGS)
+          </Paragraph>
+          <List>
+            <ListItem>Beurre / crème fraiche / fromages</ListItem>
+            <ListItem>Huile de palme</ListItem>
+            <ListItem>Charcuterie</ListItem>
+            <ListItem>Pâtisseries / Chocolat …</ListItem>
+          </List>
+        </div>
+      </Grid>
+    </PageContainer>
+  );
+};
+
+export default Page;
