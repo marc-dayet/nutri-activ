0eaba2dfdbfac14607544a692a5251baae073181
Clément DOUIN <soywod@users.noreply.github.com>
2020-11-28 11:35:57 +0100

    integrate m0c1p6

---
+++ src/modules/module-0/chapter-1/page-6.tsx
@@ -0,0 +1,28 @@
+import React, {FC} from "react";
+import PageContainer from "../../page";
+import {Title, Paragraph, Img} from "../../page-components";
+import cs from "./page-6.module.scss";
+import img1 from "./page-6-1.png";
+import img2 from "./page-6-2.png";
+
+const Page: FC = () => {
+  return (
+    <PageContainer>
+      <Title>Présentation des ateliers Nutri Activ'</Title>
+      <Paragraph className={cs.paragraph} bold padder>
+        Les ateliers sont composés de 6 modules :
+      </Paragraph>
+      <Paragraph padder>
+        <Img src={img1} mode="avatar-lg" />
+      </Paragraph>
+      <div className={cs.grid}>
+        <img className={cs.img} src={img2} alt="" />
+        <Paragraph>
+          Pour information, la formation en distanciel va suivre le même déroulé.
+        </Paragraph>
+      </div>
+    </PageContainer>
+  );
+};
+
+export default Page;
