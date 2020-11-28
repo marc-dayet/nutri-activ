77f763b1440416332078edc66f37498d169d0393
Clément DOUIN <soywod@users.noreply.github.com>
2020-11-28 10:33:50 +0100

    integrate m0c1p3

---
+++ src/modules/module-0/chapter-1/page-3.tsx
@@ -0,0 +1,54 @@
+import React, {FC} from "react";
+
+import PageContainer from "../../page";
+import {Title, Paragraph} from "../../page-components";
+import cs from "./page-3.module.scss";
+import img1 from "./page-3-1.png";
+import img2 from "./page-3-1.png";
+import img3 from "./page-3-1.png";
+import img4 from "./page-3-1.png";
+import img5 from "./page-3-1.png";
+import img6 from "./page-3-1.png";
+
+const Page: FC = () => {
+  return (
+    <PageContainer>
+      <Title>Le parcours de formation</Title>
+      <div className={cs.grid}>
+        <img className={cs.img} src={img1} alt="" />
+        <Paragraph align="left" bold>
+          Vous avez déjà réalisé la procédure de sélection.
+        </Paragraph>
+        <img className={cs.img} src={img2} alt="" />
+        <Paragraph align="left" bold>
+          Vous entrez actuellement dans la formation en distanciel pour a<br /> minima 3 semaines.
+        </Paragraph>
+        <img className={cs.img} src={img3} alt="" />
+        <Paragraph align="left" bold>
+          Une web-conférence sera organisée pour répondre aux questions <br />
+          sur le contenu du distanciel.
+        </Paragraph>
+        <img className={cs.img} src={img4} alt="" />
+        <Paragraph align="left" bold>
+          Un point de situation sera réalisé avec le formateur avant votre <br />
+          passage à l’étape suivante.
+        </Paragraph>
+        <img className={cs.img} src={img5} alt="" />
+        <Paragraph align="left" bold>
+          Pour terminer, vous participerez à la formation présentielle pour <br />
+          s'approprier les techniques d’animation sur 3 jours.
+        </Paragraph>
+      </div>
+      <br />
+      <div className={cs.grid}>
+        <img className={cs.img} src={img6} alt="" />
+        <Paragraph className={cs.subparagraph} align="left">
+          Vous trouverez le descriptif de l’ensemble de la formation dans le <br />
+          livret « Présentation de la formation » que vous avez déjà reçu.
+        </Paragraph>
+      </div>
+    </PageContainer>
+  );
+};
+
+export default Page;
