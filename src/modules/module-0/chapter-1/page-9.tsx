caffd94e3479ed8273cd1298c475871ec5b8fd5c
Clément DOUIN <soywod@users.noreply.github.com>
2020-11-28 12:39:19 +0100

    integrate m0c1p9

---
+++ src/modules/module-0/chapter-1/page-9.tsx
@@ -0,0 +1,45 @@
+import React, {FC} from "react";
+
+import PageContainer from "../../page";
+import {Title, Paragraph, Img, List, ListItem} from "../../page-components";
+import cs from "./page-9.module.scss";
+import img1 from "./page-9-1.png";
+import img2 from "./page-9-2.png";
+
+const Page: FC = () => {
+  return (
+    <PageContainer>
+      <Title>Déroulé de la formation en distanciel</Title>
+      <Paragraph bold padder>
+        Au cours de chaque module, si vous avez des questions ou des remarques, <br />
+        vous pouvez les inscrire via un lien dédié. Deux options sont possibles :
+      </Paragraph>
+      <div className={cs.grid}>
+        <Img src={img1} mode="avatar-lg" />
+        <List>
+          <ListItem>
+            <strong>Vous souhaitez le faire à la fin de chaque module</strong> : utilisez
+            impérativement le même ordinateur, cliquez sur « terminer » tout en bas du questionnaire
+            afin d’enregistrer vos réponses et <strong>pouvoir y revenir</strong>.
+          </ListItem>
+          <br />
+
+          <ListItem>
+            <strong>Vous souhaitez le faire à la fin du distanciel</strong> : notez vos questions
+            tout au long du distanciel puis une fois les modules terminés, suivez le lien pour
+            envoyer vos questions. Cliquez ensuite sur « terminer » pour les enregistrer.
+          </ListItem>
+        </List>
+      </div>
+      <div className={cs.subgrid}>
+        <img className={cs.img} src={img2} alt="" />
+        <Paragraph align="left">
+          Tous les commentaires saisis et enregistrés seront envoyés au formateur <br />
+          pour l’étape suivante : la web-conférence.
+        </Paragraph>
+      </div>
+    </PageContainer>
+  );
+};
+
+export default Page;
