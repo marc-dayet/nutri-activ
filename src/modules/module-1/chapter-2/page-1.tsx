2b16db1eb5aead4f428a4ecabc250fb156b42e50
Clément DOUIN <soywod@users.noreply.github.com>
2020-11-28 13:50:28 +0100

    integrate m1c2p2

---
+++ src/modules/module-1/chapter-2/page-1.tsx
@@ -0,0 +1,19 @@
+import React, {FC} from "react";
+
+import PageContainer from "../../page";
+import {Title, Subtitle, Paragraph, Img} from "../../page-components";
+import img from "./page-1.png";
+
+const Page: FC = () => {
+  return (
+    <PageContainer>
+      <Title>Les grands déterminants du comportement alimentaire</Title>
+      <Subtitle>Une multitude de facteurs viennent impacter nos choix alimentaires.</Subtitle>
+      <Paragraph>
+        <Img src={img} mode="fullscreen" />
+      </Paragraph>
+    </PageContainer>
+  );
+};
+
+export default Page;
